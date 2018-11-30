1e407df8710cb5346a768fd4714231948a7e226f
Jay Logue <jaylogue@google.com>
2018-11-30 12:12:59 -0800

    WIP: Device layer port to nRF52840
    -- Significant restructing of NRFConfig class to provide utility methods using the
    Nordic FDS API.  The most important changes were 1) exposing the OpenRecord() and
    DoAsyncFDSOp() methods and 2) the addition of the ForEachRecord() method.
    
    -- Initial implementation of the GroupKeyStoreImpl class for the Nordic platform.
    This code relies upon the new features of NRFConfig described above.

--- src/adaptations/device-layer/nRF5/GroupKeyStoreImpl.cpp
+++ src/adaptations/device-layer/nRF5/GroupKeyStoreImpl.cpp
@@ -19,7 +19,7 @@
 /**
  *    @file
  *          Provides an implementation of the Weave GroupKeyStore interface
- *          for the ESP32 platform.
+ *          for platforms based on the Nordic nRF5 SDK.
  */
 
 #include <Weave/DeviceLayer/internal/WeaveDeviceLayerInternal.h>
@@ -36,8 +36,50 @@ namespace Internal {
 
 WEAVE_ERROR GroupKeyStoreImpl::RetrieveGroupKey(uint32_t keyId, WeaveGroupKey & key)
 {
-    // TODO: implement me
-    return WEAVE_ERROR_KEY_NOT_FOUND;
+    WEAVE_ERROR err;
+
+    // Iterate over all the GroupKey records looking for a matching key...
+    err = ForEachRecord(GetFileId(kConfigKey_GroupKey), GetRecordKey(kConfigKey_GroupKey),
+              [keyId, &key](const fds_flash_record_t & rec, bool & deleteRec) -> WEAVE_ERROR
+              {
+                  uint32_t curKeyId;
+                  WEAVE_ERROR err2 = WEAVE_NO_ERROR;
+
+                  // Decode the Weave key id for the current key.
+                  err2 = DecodeGroupKeyId((const uint8_t *)rec.p_data, rec.p_header->length_words * kFDSWordSize, curKeyId);
+                  SuccessOrExit(err2);
+
+                  // If it matches the key we're looking for...
+                  if (curKeyId == keyId)
+                  {
+                      // Decode the associated key data.
+                      err2 = DecodeGroupKey((const uint8_t *)rec.p_data, rec.p_header->length_words * kFDSWordSize, key);
+                      SuccessOrExit(err2);
+
+                      // End the iteration by returning a WEAVE_END_OF_INPUT result.
+                      ExitNow(err2 = WEAVE_END_OF_INPUT);
+                  }
+
+              exit:
+                  return err2;
+              }
+          );
+
+    // If a matching key was found, return success.
+    if (err == WEAVE_END_OF_INPUT)
+    {
+        err = WEAVE_NO_ERROR;
+    }
+
+    // If no match was found, return WEAVE_ERROR_KEY_NOT_FOUND.
+    else if (err == WEAVE_NO_ERROR)
+    {
+        err = WEAVE_ERROR_KEY_NOT_FOUND;
+    }
+    SuccessOrExit(err);
+
+exit:
+    return err;
 }
 
 WEAVE_ERROR GroupKeyStoreImpl::StoreGroupKey(const WeaveGroupKey & key)
@@ -48,28 +90,120 @@ WEAVE_ERROR GroupKeyStoreImpl::StoreGroupKey(const WeaveGroupKey & key)
 
 WEAVE_ERROR GroupKeyStoreImpl::DeleteGroupKey(uint32_t keyId)
 {
-    // TODO: implement me
-    return WEAVE_NO_ERROR;
+    WEAVE_ERROR err;
+
+    // Iterate over all the GroupKey records looking for matching keys...
+    err = ForEachRecord(GetFileId(kConfigKey_GroupKey), GetRecordKey(kConfigKey_GroupKey),
+              [keyId](const fds_flash_record_t & rec, bool & deleteRec) -> WEAVE_ERROR
+              {
+                  uint32_t curKeyId;
+                  WEAVE_ERROR err2;
+
+                  // Decode the Weave key id for the current group key.
+                  err2 = DecodeGroupKeyId((const uint8_t *)rec.p_data, rec.p_header->length_words * kFDSWordSize, curKeyId);
+                  SuccessOrExit(err2);
+
+                  // If it matches the key looking for, arrange for the record to be deleted.
+                  deleteRec = (curKeyId == keyId);
+
+              exit:
+                  return err2;
+              }
+          );
+    SuccessOrExit(err);
+
+exit:
+    return err;
 }
 
 WEAVE_ERROR GroupKeyStoreImpl::DeleteGroupKeysOfAType(uint32_t keyType)
 {
-    // TODO: implement me
-    return WEAVE_NO_ERROR;
+    WEAVE_ERROR err;
+
+    // Iterate over all the GroupKey records looking for matching keys...
+    err = ForEachRecord(GetFileId(kConfigKey_GroupKey), GetRecordKey(kConfigKey_GroupKey),
+              [keyType](const fds_flash_record_t & rec, bool & deleteRec) -> WEAVE_ERROR
+              {
+                  uint32_t curKeyId;
+                  WEAVE_ERROR err2;
+
+                  // Decode the Weave key id for the current group key.
+                  err2 = DecodeGroupKeyId((const uint8_t *)rec.p_data, rec.p_header->length_words * kFDSWordSize, curKeyId);
+                  SuccessOrExit(err2);
+
+                  // If the current key matches the type we're looking for, arrange for the
+                  // record to be deleted.
+                  deleteRec = (WeaveKeyId::GetType(curKeyId) == keyType);
+
+              exit:
+                  return err2;
+              }
+          );
+    SuccessOrExit(err);
+
+exit:
+    return err;
 }
 
 WEAVE_ERROR GroupKeyStoreImpl::EnumerateGroupKeys(uint32_t keyType, uint32_t * keyIds,
         uint8_t keyIdsArraySize, uint8_t & keyCount)
 {
-    // TODO: implement me
+    WEAVE_ERROR err;
+
     keyCount = 0;
-    return WEAVE_NO_ERROR;
+
+    // Iterate over all the GroupKey records looking for keys of the specified type...
+    err = ForEachRecord(GetFileId(kConfigKey_GroupKey), GetRecordKey(kConfigKey_GroupKey),
+              [keyType, keyIds, keyIdsArraySize, &keyCount](const fds_flash_record_t & rec, bool & deleteRec) -> WEAVE_ERROR
+              {
+                  uint32_t curKeyId;
+                  WEAVE_ERROR err2 = WEAVE_NO_ERROR;
+
+                  // Decode the Weave key id for the current key.
+                  err2 = DecodeGroupKeyId((const uint8_t *)rec.p_data, rec.p_header->length_words * kFDSWordSize, curKeyId);
+                  SuccessOrExit(err2);
+
+                  // If the current key matches the type we're looking for, add it to the keyIds array.
+                  if (WeaveKeyId::GetType(curKeyId) == keyType)
+                  {
+                      keyIds[keyCount++] = curKeyId;
+
+                      // Stop iterating if there's no more room in the keyIds array.
+                      VerifyOrExit(keyCount < keyIdsArraySize, err2 = WEAVE_ERROR_BUFFER_TOO_SMALL);
+                  }
+
+              exit:
+                  return err2;
+              }
+          );
+
+    // Simply return a truncated list if there are more matching keys than will fit in the array.
+    if (err == WEAVE_ERROR_BUFFER_TOO_SMALL)
+    {
+        err = WEAVE_NO_ERROR;
+    }
+    SuccessOrExit(err);
+
+exit:
+    return err;
 }
 
 WEAVE_ERROR GroupKeyStoreImpl::Clear(void)
 {
-    // TODO: implement me
-    return WEAVE_NO_ERROR;
+    WEAVE_ERROR err;
+
+    // Iterate over all GroupKey records deleting each one.
+    err = ForEachRecord(GetFileId(kConfigKey_GroupKey), GetRecordKey(kConfigKey_GroupKey),
+              [](const fds_flash_record_t & rec, bool & deleteRec) -> WEAVE_ERROR
+              {
+                  deleteRec = true;
+                  return WEAVE_NO_ERROR;
+              }
+          );
+    SuccessOrExit(err);
+
+exit:
+    return err;
 }
 
 WEAVE_ERROR GroupKeyStoreImpl::RetrieveLastUsedEpochKeyId(void)
@@ -92,8 +226,58 @@ WEAVE_ERROR GroupKeyStoreImpl::StoreLastUsedEpochKeyId(void)
 
 WEAVE_ERROR GroupKeyStoreImpl::Init()
 {
-    // TODO: implement me
+    // Nothing to do
     return WEAVE_NO_ERROR;
+}
+
+WEAVE_ERROR GroupKeyStoreImpl::EncodeGroupKey(WeaveGroupKey & key, uint8_t * buf, size_t bufSize, size_t & encodedKeyLen)
+{
+    WEAVE_ERROR err = WEAVE_NO_ERROR;
+    uint8_t * p = buf;
+
+    VerifyOrExit(bufSize >= kFixedEncodedKeySize + key.KeyLen, err = WEAVE_ERROR_BUFFER_TOO_SMALL);
+
+    Encoding::LittleEndian::Write32(p, key.KeyId);
+    Encoding::LittleEndian::Write32(p, key.StartTime);
+    Encoding::Write8(p, key.KeyLen);
+    memcpy(p, key.Key, key.KeyLen);
+    p += key.KeyLen;
+
+    encodedKeyLen = p - buf;
+
+exit:
+    return err;
+}
+
+WEAVE_ERROR GroupKeyStoreImpl::DecodeGroupKey(const uint8_t * encodedKey, size_t encodedKeyLen, WeaveGroupKey & key)
+{
+    WEAVE_ERROR err = WEAVE_NO_ERROR;
+    const uint8_t * p = encodedKey;
+
+    VerifyOrExit(encodedKeyLen >= kFixedEncodedKeySize, err = WEAVE_ERROR_INVALID_ARGUMENT);
+
+    key.KeyId = Encoding::LittleEndian::Read32(p);
+    key.StartTime = Encoding::LittleEndian::Read32(p);
+    key.KeyLen = Encoding::Read8(p);
+
+    VerifyOrExit(encodedKeyLen >= kFixedEncodedKeySize + key.KeyLen, err = WEAVE_ERROR_INVALID_ARGUMENT);
+
+    memcpy(key.Key, p, key.KeyLen);
+
+exit:
+    return err;
+}
+
+WEAVE_ERROR GroupKeyStoreImpl::DecodeGroupKeyId(const uint8_t * encodedKey, size_t encodedKeyLen, uint32_t & keyId)
+{
+    WEAVE_ERROR err = WEAVE_NO_ERROR;
+
+    VerifyOrExit(encodedKeyLen >= kFixedEncodedKeySize, err = WEAVE_ERROR_INVALID_ARGUMENT);
+
+    keyId = Encoding::LittleEndian::Get32(encodedKey);
+
+exit:
+    return err;
 }
 
 } // namespace Internal
