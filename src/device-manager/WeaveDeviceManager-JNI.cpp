openweave/openweave-core

commit 61384990c41814dceeeed4e03a590884986ddb64

Improve Weave Device Manager (java) to allow Thread network information objects with explcit Thread PAN identifier and channel values.

src/device-manager/WeaveDeviceManager-JNI.cpp
@@ -1,5 +1,6 @@
 /*
  *
+ *    Copyright (c) 2019 Google LLC
  *    Copyright (c) 2013-2017 Nest Labs, Inc.
  *    All rights reserved.
  *
@@ -3013,6 +3014,7 @@ WEAVE_ERROR J2N_NetworkInfo(JNIEnv *env, jobject inNetworkInfo, NetworkInfo& out
 {
     WEAVE_ERROR err = WEAVE_NO_ERROR;
     jlong longVal;
+    jint intVal;
     jshort shortVal;
     uint32_t len;
     int enumVal;
@@ -3054,6 +3056,14 @@ WEAVE_ERROR J2N_NetworkInfo(JNIEnv *env, jobject inNetworkInfo, NetworkInfo& out
     err = J2N_ByteArrayFieldVal(env, inNetworkInfo, "ThreadNetworkKey", outNetworkInfo.ThreadNetworkKey, outNetworkInfo.ThreadNetworkKeyLen);
     SuccessOrExit(err);
 
+    err = J2N_IntFieldVal(env, inNetworkInfo, "ThreadPANId", intVal);
+    SuccessOrExit(err);
+    outNetworkInfo.ThreadPANId = intVal;
+
+    err = J2N_IntFieldVal(env, inNetworkInfo, "ThreadChannel", intVal);
+    SuccessOrExit(err);
+    outNetworkInfo.ThreadChannel = intVal;
+
     err = J2N_ShortFieldVal(env, inNetworkInfo, "WirelessSignalStrength", shortVal);
     SuccessOrExit(err);
     outNetworkInfo.WirelessSignalStrength = shortVal;
@@ -3069,7 +3079,7 @@ WEAVE_ERROR N2J_NetworkInfo(JNIEnv *env, const NetworkInfo& inNetworkInfo, jobje
     jstring wifiSSID = NULL;
     jbyteArray wifiKey = NULL;
     jstring threadNetName = NULL;
-    jbyteArray threadPANId = NULL;
+    jbyteArray threadExtPANId = NULL;
     jbyteArray threadKey = NULL;
 
     if (inNetworkInfo.WiFiSSID != NULL)
@@ -3092,7 +3102,7 @@ WEAVE_ERROR N2J_NetworkInfo(JNIEnv *env, const NetworkInfo& inNetworkInfo, jobje
 
     if (inNetworkInfo.ThreadExtendedPANId != NULL)
     {
-        err = N2J_ByteArray(env, inNetworkInfo.ThreadExtendedPANId, 8, threadPANId);
+        err = N2J_ByteArray(env, inNetworkInfo.ThreadExtendedPANId, 8, threadExtPANId);
         SuccessOrExit(err);
     }
 
@@ -3115,14 +3125,16 @@ WEAVE_ERROR N2J_NetworkInfo(JNIEnv *env, const NetworkInfo& inNetworkInfo, jobje
                                                  (jint)inNetworkInfo.WiFiSecurityType,
                                                  wifiKey,
                                                  threadNetName,
-                                                 threadPANId,
+                                                 threadExtPANId,
                                                  threadKey,
+                                                 (jint)inNetworkInfo.ThreadPANId,
+                                                 (jbyte)inNetworkInfo.ThreadChannel,
                                                  (jshort)inNetworkInfo.WirelessSignalStrength);
     VerifyOrExit(!env->ExceptionCheck(), err = WDM_JNI_ERROR_EXCEPTION_THROWN);
 
 exit:
     env->DeleteLocalRef(threadKey);
-    env->DeleteLocalRef(threadPANId);
+    env->DeleteLocalRef(threadExtPANId);
     env->DeleteLocalRef(threadNetName);
     env->DeleteLocalRef(wifiKey);
     env->DeleteLocalRef(wifiSSID);

src/device-manager/java/src/nl/Weave/DeviceManager/NetworkInfo.java
@@ -1,6 +1,7 @@
 /*
  *
- *    Copyright (c) 013-2017 Nest Labs, Inc.
+ *    Copyright (c) 2019 Google LLC
+ *    Copyright (c) 2013-2017 Nest Labs, Inc.
  *    All rights reserved.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
@@ -23,10 +24,19 @@ package nl.Weave.DeviceManager;
  */
 public class NetworkInfo
 {
+    /** The distinguished "not specified" network identifier. */
+    public static final long NETWORK_ID_NOTSPECIFIED = -1;
+
+    /** The distinguished "not specified" Thread PAN identifier. */
+    public static final int THREAD_PANID_NOTSPECIFIED = -1;
+
+    /** The distinguished "not specified" Thread channel number. */
+    public static final int THREAD_CHANNEL_NOTSPECIFIED = -1;
+
     public NetworkInfo()
     {
         NetworkType = nl.Weave.DeviceManager.NetworkType.NotSpecified;
-        NetworkId = -1;
+        NetworkId = NETWORK_ID_NOTSPECIFIED;
         WiFiSSID = null;
         WiFiMode = nl.Weave.DeviceManager.WiFiMode.NotSpecified;
         WiFiRole = nl.Weave.DeviceManager.WiFiRole.NotSpecified;
@@ -35,6 +45,8 @@ public class NetworkInfo
         ThreadNetworkName = null;
         ThreadExtendedPANId = null;
         ThreadNetworkKey = null;
+        ThreadPANId = THREAD_PANID_NOTSPECIFIED;
+        ThreadChannel = THREAD_CHANNEL_NOTSPECIFIED;
         WirelessSignalStrength = Short.MIN_VALUE;
     }
 
@@ -79,6 +91,15 @@ public class NetworkInfo
          */
     public byte[] ThreadNetworkKey;
 
+    /** The 16-bit Thread PAN ID, or kThreadPANId_NotSpecified 
+         */
+    public int ThreadPANId;
+
+    /** The current channel (currently [11..26]) on which the Thread
+         * network operates, or kThreadChannel_NotSpecified
+         */
+    public int ThreadChannel;
+
     /** The signal strength of the network, in dBm, or Short.MIN_VALUE if not available/applicable.
          */
     public short WirelessSignalStrength;
@@ -96,20 +117,29 @@ public class NetworkInfo
         return netInfo;
     }
 
-    public static NetworkInfo MakeThread(String threadNetworkName, byte[] threadExtendedPANId, byte[] threadNetworkKey)
+    public static NetworkInfo MakeThread(String threadNetworkName, byte[] threadExtendedPANId, byte[] threadNetworkKey,
+                                         int threadPANId, int threadChannel)
     {
         NetworkInfo netInfo = new NetworkInfo();
         netInfo.NetworkType = nl.Weave.DeviceManager.NetworkType.Thread;
         netInfo.ThreadNetworkName = threadNetworkName;
         netInfo.ThreadExtendedPANId = threadExtendedPANId;
         netInfo.ThreadNetworkKey = threadNetworkKey;
+        netInfo.ThreadPANId = threadPANId;
+        netInfo.ThreadChannel = threadChannel;
         return netInfo;
+    }
+
+    public static NetworkInfo MakeThread(String threadNetworkName, byte[] threadExtendedPANId, byte[] threadNetworkKey)
+    {
+        return MakeThread(threadNetworkName, threadExtendedPANId, threadNetworkKey,
+                          THREAD_PANID_NOTSPECIFIED, THREAD_CHANNEL_NOTSPECIFIED);
     }
 
     public static NetworkInfo Make(int networkType, long networkId,
                                    String wifiSSID, int wifiMode, int wifiRole, int wifiSecurityType, byte[] wifiKey,
                                    String threadNetworkName, byte[] threadExtendedPANId, byte[] threadNetworkKey,
-                                   short wirelessSignalStrength)
+                                   short wirelessSignalStrength, int threadPANId, int threadChannel)
     {
         NetworkInfo netInfo = new NetworkInfo();
         netInfo.NetworkType = nl.Weave.DeviceManager.NetworkType.fromVal(networkType);
@@ -122,7 +152,19 @@ public class NetworkInfo
         netInfo.ThreadNetworkName = threadNetworkName;
         netInfo.ThreadExtendedPANId = threadExtendedPANId;
         netInfo.ThreadNetworkKey = threadNetworkKey;
+        netInfo.ThreadPANId = threadPANId;
+        netInfo.ThreadChannel = threadChannel;
         netInfo.WirelessSignalStrength = wirelessSignalStrength;
         return netInfo;
+    }
+
+    public static NetworkInfo Make(int networkType, long networkId,
+                                   String wifiSSID, int wifiMode, int wifiRole, int wifiSecurityType, byte[] wifiKey,
+                                   String threadNetworkName, byte[] threadExtendedPANId, byte[] threadNetworkKey,
+                                   short wirelessSignalStrength)
+    {
+        return Make(networkType, networkId, wifiSSID, wifiMode, wifiRole, wifiSecurityType, wifiKey, threadNetworkName,
+                    threadExtendedPANId, threadNetworkKey, wirelessSignalStrength, THREAD_PANID_NOTSPECIFIED,
+                    THREAD_CHANNEL_NOTSPECIFIED);
     }
 }

src/device-manager/java/src/nl/Weave/DeviceManager/TestMain.java
@@ -1,6 +1,7 @@
 /*
 
-    Copyright (c) 013-2017 Nest Labs, Inc.
+    Copyright (c) 2019 Google LLC
+    Copyright (c) 2013-2017 Nest Labs, Inc.
     All rights reserved.
 
     Licensed under the Apache License, Version 2.0 (the "License");
@@ -178,7 +179,9 @@ public class TestMain implements WeaveDeviceManager.CompletionHandler
         System.out.println("    Adding new Thread network...");
         networkInfo = NetworkInfo.MakeThread("Thread-Test",
                                         DatatypeConverter.parseHexBinary("0102030405060708"),
-                                        "akey".getBytes());
+                                        "akey".getBytes(),
+                                        0x1234,
+                                        (byte)21);
         DeviceMgr.beginAddNetwork(networkInfo);
         ExpectSuccess("AddNetwork");
         System.out.println("AddNetwork Test Succeeded");

commit 6f2ef79a3b809f14d8c9b3d0056681a10c236552

Fix a crash when decoding NetworkInfo from JNI
Regression since 61384990c4; when the N2J_NetworkInfo method was updated to include the additional Thread network parameters, the order of parameters was incorrect. The Thread PAN ID was being supplied in place of the wireless signal strength, which frequently exceeds the limit of a jshort.

src/device-manager/WeaveDeviceManager-JNI.cpp
@@ -3127,9 +3127,9 @@ WEAVE_ERROR N2J_NetworkInfo(JNIEnv *env, const NetworkInfo& inNetworkInfo, jobje
                                                  threadNetName,
                                                  threadExtPANId,
                                                  threadKey,
+                                                 (jshort)inNetworkInfo.WirelessSignalStrength,
                                                  (jint)inNetworkInfo.ThreadPANId,
-                                                 (jbyte)inNetworkInfo.ThreadChannel,
-                                                 (jshort)inNetworkInfo.WirelessSignalStrength);
+                                                 (jbyte)inNetworkInfo.ThreadChannel);
     VerifyOrExit(!env->ExceptionCheck(), err = WDM_JNI_ERROR_EXCEPTION_THROWN);
 
 exit: