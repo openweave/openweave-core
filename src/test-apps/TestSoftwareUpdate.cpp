bb02d0caa07877328493cf934ed97250872f46fc
Robert Szewczyk <rszewczyk@nestlabs.com>
2019-03-12 18:30:52 -0700

    More cleanup of SoftwareUpdate profile code
    * remove obsolete test -- all the test cases have been moved to
      TestSoftwareUpdate.cpp
    * change to the proper copyright owner

--- src/test-apps/TestSoftwareUpdate.cpp
+++ src/test-apps/TestSoftwareUpdate.cpp
@@ -1,6 +1,6 @@
 /*
  *
- *    Copyright (c) 2017 Nest Labs, Inc.
+ *    Copyright (c) 2019 Google LLC.
  *    All rights reserved.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
