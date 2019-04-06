68879939bbf5818674d1c30edfcb8073dfdf806c
Jay Logue <jaylogue@google.com>
2019-04-06 07:41:33 -0700

    Changes to start-jlink-gdb-server.sh script
    -- The J-Link GDB server will occasionally close and then re-open the
    RTT logging port when the device reboots.  To prevent the script from
    exiting when this happens, the script now repeatly telnets to the port
    until it notices that the GDB server itself has exited.
    
    -- Fixed bug in the logic that detects whether the server is listening
    on the RTT port.

--- openweave-nrf52840-bringup/start-jlink-gdb-server.sh
+++ openweave-nrf52840-bringup/start-jlink-gdb-server.sh
@@ -7,28 +7,26 @@ DEVICE_TYPE=NRF52840_XXAA
 ${JLINK_GDB_SERVER} -device ${DEVICE_TYPE} -if SWD -speed 4000 $* > >(exec sed -e 's/^/JLinkGDBServer: /') 2>&1 &
 GDB_SERVER_PID=$!
 
-# Wait for GDB server to begin listening on RTT port
-while ! (cat /proc/net/tcp | egrep -q '^\s+[[:digit:]]+:\s+0100007F:4A4D'); 
+# Repeatedly open a telnet connection to the GDB server's RTT port until
+# the user kills the GDB server with an interrupt character.
+while true;
 do
-    # Fail if the GDB server exits unexpectedly.
-    if ! (kill -0 ${GDB_SERVER_PID} >/dev/null 2>&1); then
-        echo ""
-        exit
-    fi
+    # Wait for GDB server to begin listening on RTT port
+    while ! (cat /proc/net/tcp | egrep -q '^\s+[[:digit:]]+:\s+0100007F:4A4D\s+00000000:0000\s+0A'); 
+    do
+        # Quit if the GDB server exits.
+        if ! (kill -0 ${GDB_SERVER_PID} >/dev/null 2>&1); then
+            echo ""
+            exit
+        fi
 
-    # Wait a bit.
-    sleep 0.1s
-done
+        # Wait a bit.
+        sleep 0.1s
 
-# Telnet to RTT port; wait for user to close telnet session.
-telnet localhost 19021
+    done
 
-# Signal GDB server to shutdown
-kill -INT ${GDB_SERVER_PID}
+    # Telnet to RTT port.
+    telnet localhost 19021
 
-# Wait for GDB server to shutdown
-while (kill -0 ${GDB_SERVER_PID} >/dev/null 2>&1); 
-do
-    sleep 0.1s
 done
 
