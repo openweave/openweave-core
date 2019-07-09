fd6c0a4334b2d6e582b599bb2aabc70c9637842b
Jennie Xie <jexie@google.com>
2019-07-09 14:55:03 -0700

    fix inet dns test

--- src/test-apps/happy/tests/standalone/inet/topology/two_nodes_wifi.sh
+++ src/test-apps/happy/tests/standalone/inet/topology/two_nodes_wifi.sh
@@ -25,5 +25,6 @@ happy-network-address --id Home --add 10.0.1.0
 happy-node-add node01
 happy-node-add --ap onhub
 happy-node-join onhub Home
-happy-node-join --tap node01 Home
+happy-node-join node01 Home
+happy-network-route --prefix 10.0.1.0 Home onhub
 happy-internet --node onhub --interface ${IFACE} --isp ${IFACE//[0-9]/} --seed 249
