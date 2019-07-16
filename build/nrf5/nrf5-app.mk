d52f40993ac9c816917b57d5b82bf0f2bc29ab07
Jay Logue <jaylogue@google.com>
2019-07-16 12:05:16 -0700

    Updated nRF5 makefiles to support nRF5 SDK 3.1

--- build/nrf5/nrf5-app.mk
+++ build/nrf5/nrf5-app.mk
@@ -163,7 +163,7 @@ NRF5_SDK_TITLE = nRF5 SDK for Thread and Zigbee
 
 NRF5_SDK_VERSION_SED_EXP = /^${NRF5_SDK_TITLE} v[0-9.]+.*$$/ { s/.*v([0-9.]+).*/\1/; s/[.]/ /g; p; q; }
 
-NRF5_SDK_MIN_VERSION = 3.0.0
+NRF5_SDK_MIN_VERSION = 3.1.0
 
 
 # ==================================================
