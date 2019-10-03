9e86faa2b6b036b8bfde267e2cd6db06cfb61895
yunhanw <yunhanw@google.com>
2019-10-03 00:45:06 -0700

    add GenericTraitCatalogImp

---
+++ src/lib/profiles/data-management/GenericTraitCatalogImp.h
@@ -0,0 +1,29 @@
+/*
+ *
+ *    Copyright (c) 2019 Google, LLC.
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+#ifndef _WEAVE_DATA_MANAGEMENT_GENERIC_TRAIT_CATALOG_IMP_CURRENT_H
+#define _WEAVE_DATA_MANAGEMENT_GENERIC_TRAIT_CATALOG_IMP_CURRENT_H
+
+#include <Weave/Profiles/data-management/WdmManagedNamespace.h>
+
+#if WEAVE_CONFIG_DATA_MANAGEMENT_NAMESPACE == kWeaveManagedNamespace_Current
+#include <Weave/Profiles/data-management/Current/GenericTraitCatalogImp.h>
+#else
+#error "WEAVE_CONFIG_DATA_MANAGEMENT_NAMESPACE defined, but not as namespace kWeaveManagedNamespace_Current"
+#endif // WEAVE_CONFIG_DATA_MANAGEMENT_NAMESPACE == kWeaveManagedNamespace_Current
+
+#endif // _WEAVE_DATA_MANAGEMENT_GENERIC_TRAIT_CATALOG_IMP_CURRENT_H
