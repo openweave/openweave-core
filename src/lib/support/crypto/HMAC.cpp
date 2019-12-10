0d17da14d2cc19b8d246d412a92297f20eeb8ed1
Evgeni Margolis <emargolis@google.com>
2019-12-09 16:51:55 -0800

    Add HMAC signature support.
    Defined types, objects, and methods for working with HMAC signatures.

--- src/lib/support/crypto/HMAC.cpp
+++ src/lib/support/crypto/HMAC.cpp
@@ -1,5 +1,6 @@
 /*
  *
+ *    Copyright (c) 2019 Google LLC.
  *    Copyright (c) 2013-2017 Nest Labs, Inc.
  *    All rights reserved.
  *
@@ -32,10 +33,13 @@
 
 #include "WeaveCrypto.h"
 #include "HMAC.h"
+#include <Weave/Support/CodeUtils.h>
 
 namespace nl {
 namespace Weave {
 namespace Crypto {
+
+using namespace nl::Weave::TLV;
 
 template <class H>
 HMAC<H>::HMAC()
@@ -138,6 +142,155 @@ void HMAC<H>::Reset()
 
 template class HMAC<Platform::Security::SHA1>;
 template class HMAC<Platform::Security::SHA256>;
+
+
+/**
+ * Compares with another HMAC signature.
+ *
+ * @param[in] other             The EncodedHMACSignature object with which signature should be compared.
+ *
+ * @retval true                 The signatures are equal.
+ * @retval false                The signatures are not equal.
+ */
+bool EncodedHMACSignature::IsEqual(const EncodedHMACSignature& other) const
+{
+    return (Sig != NULL &&
+            other.Sig != NULL &&
+            Len == other.Len &&
+            memcmp(Sig, other.Sig, Len) == 0);
+}
+
+/**
+ * Reads the signature as a Weave HMACSignature structure from the specified TLV reader.
+ *
+ * @param[in] reader            The TLVReader object from which the encoded signature should
+ *                              be read.
+ *
+ * @retval #WEAVE_NO_ERROR      If the operation succeeded.
+ * @retval other                Other Weave error codes related to signature reading.
+ */
+WEAVE_ERROR EncodedHMACSignature::ReadSignature(TLVReader& reader)
+{
+    WEAVE_ERROR err;
+
+    VerifyOrExit(reader.GetType() == kTLVType_ByteString, err = WEAVE_ERROR_WRONG_TLV_TYPE);
+
+    err = reader.GetDataPtr(const_cast<const uint8_t *&>(Sig));
+    SuccessOrExit(err);
+
+    Len = reader.GetLength();
+
+exit:
+    return err;
+}
+
+/**
+ * Generate and encode a Weave HMAC signature
+ *
+ * Computes an HMAC signature for a given data using secret key and writes the signature
+ * as a Weave HMACSignature structure to the specified TLV writer with the given tag.
+ *
+ * @param[in] sigAlgoOID        Algorithm OID to be used to generate HMAC signature.
+ * @param[in] writer            The TLVWriter object to which the encoded signature should
+ *                              be written.
+ * @param[in] tag               TLV tag to be associated with the encoded signature structure.
+ * @param[in] data              A buffer containing the data to be signed.
+ * @param[in] dataLen           The length in bytes of the data.
+ * @param[in] key               A buffer containing the secret key to be used to generate HMAC
+ *                              signature.
+ * @param[in] keyLen            The length in bytes of the secret key.
+ *
+ * @retval #WEAVE_NO_ERROR      If the operation succeeded.
+ * @retval other                Other Weave error codes related to the signature encoding.
+ */
+WEAVE_ERROR GenerateAndEncodeWeaveHMACSignature(OID sigAlgoOID,
+                                                TLVWriter& writer, uint64_t tag,
+                                                const uint8_t * data, uint16_t dataLen,
+                                                const uint8_t * key, uint16_t keyLen)
+{
+    WEAVE_ERROR err;
+    HMACSHA256 hmac;
+    uint8_t hmacSig[HMACSHA256::kDigestLength];
+    EncodedHMACSignature sig;
+
+    // Current implementation only supports HMACWithSHA256 signature algorithm.
+    VerifyOrExit(sigAlgoOID == ASN1::kOID_SigAlgo_HMACWithSHA256, err = WEAVE_ERROR_UNSUPPORTED_SIGNATURE_TYPE);
+
+    // Generate the MAC.
+    hmac.Begin(key, keyLen);
+    hmac.AddData(data, dataLen);
+    hmac.Finish(hmacSig);
+
+    sig.Sig = hmacSig;
+    sig.Len = HMACSHA256::kDigestLength;
+
+    // Encode an HMACSignature value into the supplied writer.
+    err = sig.WriteSignature(writer, tag);
+    SuccessOrExit(err);
+
+exit:
+    return err;
+}
+
+/**
+ * Verify a Weave HMAC signature.
+ *
+ * Verifies an HMAC signature using given data and a secret key to be used to verify the signature.
+ *
+ * @param[in] sigAlgoOID        Algorithm OID to be used to generate HMAC signature.
+ * @param[in] data              A buffer containing the data to be signed.
+ * @param[in] dataLen           The length in bytes of the data.
+ * @param[in] sig               Encoded HMAC signature to be verified.
+ * @param[in] key               A buffer containing the secret key to be used to generate HMAC
+ *                              signature.
+ * @param[in] keyLen            The length in bytes of the secret key.
+ *
+ * @retval #WEAVE_NO_ERROR      If HMAC signature verification succeeded.
+ * @retval #WEAVE_ERROR_INVALID_SIGNATURE
+ *                              If HMAC signature verification failed.
+ */
+WEAVE_ERROR VerifyHMACSignature(OID sigAlgoOID,
+                                const uint8_t * data, uint16_t dataLen,
+                                const EncodedHMACSignature& sig,
+                                const uint8_t * key, uint16_t keyLen)
+{
+    WEAVE_ERROR err = WEAVE_NO_ERROR;
+    HMACSHA256 hmac;
+    uint8_t hmacSig[HMACSHA256::kDigestLength];
+    EncodedHMACSignature lSig;
+
+    // Current implementation only supports HMACWithSHA256 signature algorithm.
+    VerifyOrExit(sigAlgoOID == ASN1::kOID_SigAlgo_HMACWithSHA256, err = WEAVE_ERROR_UNSUPPORTED_SIGNATURE_TYPE);
+
+    // Generate the MAC.
+    hmac.Begin(key, keyLen);
+    hmac.AddData(data, dataLen);
+    hmac.Finish(hmacSig);
+
+    lSig.Sig = hmacSig;
+    lSig.Len = HMACSHA256::kDigestLength;
+
+    VerifyOrExit(lSig.IsEqual(sig), err = WEAVE_ERROR_INVALID_SIGNATURE);
+
+exit:
+    return err;
+}
+
+// ==================== Documentation for Inline Public Members ====================
+
+/**
+ * @fn WEAVE_ERROR EncodedHMACSignature::WriteSignature(TLVWriter& writer, uint64_t tag) const
+ *
+ * Writes the signature as a Weave HMACSignature structure to the specified TLV writer
+ * with the given tag.
+ *
+ * @param[in] writer            The TLVWriter object to which the encoded signature should
+ *                              be written.
+ * @param[in] tag               TLV tag to be associated with the encoded signature structure.
+
+ * @retval #WEAVE_NO_ERROR      If the operation succeeded.
+ * @retval other                Other Weave error codes related to signature writing.
+ */
 
 } // namespace Crypto
 } // namespace Weave
