d3e9a5dc151ec7ca447c89f7e8ff7696e2c6649b
Robert Szewczyk <szewczyk@google.com>
2019-12-11 17:05:12 -0800

    Updated the schema files to reflect the latest in openweave-wdlc
    Updated the src/test-apps/schema to reflect exactly the reference
    codegen from https://github.com/openweave/openweave-wdlc.git.  This
    change brings the schema to commit
    c827473f8c762af08391667da1f99ef81bd8a22f
    
    One change required to make to the generated files is that the
    kWeaveProfileId in TestMismatchedCTrait.h must have the same value at
    the similar enum in TestCTrait.h.  There is no way in checking such
    code into WDLC, so this edit must be carried out manually.

--- src/test-apps/schema/nest/test/trait/TestETrait.h
+++ src/test-apps/schema/nest/test/trait/TestETrait.h
@@ -1,8 +1,23 @@
 
-/**
- *    Copyright (c) 2018 Nest Labs, Inc.
+/*
+ *    Copyright (c) 2019 Google LLC.
+ *    Copyright (c) 2016-2018 Nest Labs, Inc.
  *    All rights reserved.
  *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+/*
  *    THIS FILE IS GENERATED. DO NOT MODIFY.
  *
  *    SOURCE TEMPLATE: trait.cpp.h
@@ -496,6 +511,26 @@ inline bool TestENullableEvent::IsNeJPresent(void)
 }
 #endif
 
+struct TestEEmptyEvent
+{
+
+    static const nl::SchemaFieldDescriptor FieldSchema;
+
+    // Statically-known Event Struct Attributes:
+    enum {
+            kWeaveProfileId = (0x235aU << 16) | 0xfe06U,
+        kEventTypeId = 0x3U
+    };
+
+    static const nl::Weave::Profiles::DataManagement::EventSchema Schema;
+};
+
+struct TestEEmptyEvent_array {
+    uint32_t num;
+    TestEEmptyEvent *buf;
+};
+
+
 //
 // Enums
 //
@@ -506,7 +541,6 @@ enum EnumE {
     ENUM_E_VALUE_3 = 3,
     ENUM_E_VALUE_4 = 4,
 };
-
 
 } // namespace TestETrait
 } // namespace Trait
--- src/test-apps/schema/nest/test/trait/TestHTrait.cpp
+++ src/test-apps/schema/nest/test/trait/TestHTrait.cpp
@@ -1,8 +1,23 @@
 
-/**
- *    Copyright (c) 2018 Nest Labs, Inc.
+/*
+ *    Copyright (c) 2019 Google LLC.
+ *    Copyright (c) 2016-2018 Nest Labs, Inc.
  *    All rights reserved.
  *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+/*
  *    THIS FILE IS GENERATED. DO NOT MODIFY.
  *
  *    SOURCE TEMPLATE: trait.cpp
@@ -89,26 +104,6 @@ const TraitSchemaEngine TraitSchema = {
 // Event Structs
 //
 
-const nl::FieldDescriptor StructHFieldDescriptors[] =
-{
-    {
-        NULL, offsetof(StructH, sb), SET_TYPE_AND_FLAGS(nl::SerializedFieldTypeUInt32, 0), 2
-    },
-
-    {
-        NULL, offsetof(StructH, sc), SET_TYPE_AND_FLAGS(nl::SerializedFieldTypeUInt32, 0), 3
-    },
-
-};
-
-const nl::SchemaFieldDescriptor StructH::FieldSchema =
-{
-    .mNumFieldDescriptorElements = sizeof(StructHFieldDescriptors)/sizeof(StructHFieldDescriptors[0]),
-    .mFields = StructHFieldDescriptors,
-    .mSize = sizeof(StructH)
-};
-
-
 const nl::FieldDescriptor StructDictionaryFieldDescriptors[] =
 {
     {
@@ -130,6 +125,26 @@ const nl::SchemaFieldDescriptor StructDictionary::FieldSchema =
     .mNumFieldDescriptorElements = sizeof(StructDictionaryFieldDescriptors)/sizeof(StructDictionaryFieldDescriptors[0]),
     .mFields = StructDictionaryFieldDescriptors,
     .mSize = sizeof(StructDictionary)
+};
+
+
+const nl::FieldDescriptor StructHFieldDescriptors[] =
+{
+    {
+        NULL, offsetof(StructH, sb), SET_TYPE_AND_FLAGS(nl::SerializedFieldTypeUInt32, 0), 2
+    },
+
+    {
+        NULL, offsetof(StructH, sc), SET_TYPE_AND_FLAGS(nl::SerializedFieldTypeUInt32, 0), 3
+    },
+
+};
+
+const nl::SchemaFieldDescriptor StructH::FieldSchema =
+{
+    .mNumFieldDescriptorElements = sizeof(StructHFieldDescriptors)/sizeof(StructHFieldDescriptors[0]),
+    .mFields = StructHFieldDescriptors,
+    .mSize = sizeof(StructH)
 };
 
 } // namespace TestHTrait
