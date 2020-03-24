7db5ba78a56f4d8092973623faa8275b45288265
Robert Szewczyk <szewczyk@google.com>
2020-03-24 12:00:59 -0700

    Preserve original formating in profiles/{fabric-provisioning|heartbeat}

--- src/lib/profiles/fabric-provisioning/FabricProvisioning.h
+++ src/lib/profiles/fabric-provisioning/FabricProvisioning.h
@@ -57,11 +57,12 @@ using nl::Inet::IPAddress;
 /**
  * Fabric Provisioning Status Codes
  */
+// clang-format off
 enum
 {
-    kStatusCode_AlreadyMemberOfFabric           = 1,    /**< The recipient is already a member of a fabric. */
-    kStatusCode_NotMemberOfFabric               = 2,    /**< The recipient is not a member of a fabric. */
-    kStatusCode_InvalidFabricConfig             = 3     /**< The specified fabric configuration was invalid. */
+    kStatusCode_AlreadyMemberOfFabric = 1, /**< The recipient is already a member of a fabric. */
+    kStatusCode_NotMemberOfFabric     = 2, /**< The recipient is not a member of a fabric. */
+    kStatusCode_InvalidFabricConfig   = 3  /**< The specified fabric configuration was invalid. */
 };
 
 /**
@@ -70,11 +71,11 @@ enum
 enum
 {
     // Application/Device Messages
-    kMsgType_CreateFabric                       = 1,
-    kMsgType_LeaveFabric                        = 2,
-    kMsgType_GetFabricConfig                    = 3,
-    kMsgType_GetFabricConfigComplete            = 4,
-    kMsgType_JoinExistingFabric                 = 5
+    kMsgType_CreateFabric             = 1,
+    kMsgType_LeaveFabric              = 2,
+    kMsgType_GetFabricConfig          = 3,
+    kMsgType_GetFabricConfigComplete  = 4,
+    kMsgType_JoinExistingFabric       = 5
 };
 
 /**
@@ -83,28 +84,28 @@ enum
 enum
 {
     // ---- Top-level Data Elements ----
-    kTag_FabricConfig                           = 1,    /**< [ structure ] Contains provisioning information for an existing fabric.
-                                                               IMPORTANT NOTE: As a convenience to readers, all elements in a FabricConfig
-                                                               must be encoded in numeric tag order, at all levels. */
+    kTag_FabricConfig                 = 1, /**< [ structure ] Contains provisioning information for an existing fabric.
+                                                  @note @b IMPORTANT: As a convenience to readers, all elements in a FabricConfig
+                                                  must be encoded in numeric tag order, at all levels. */
 
     // ---- Context-specific Tags for FabricConfig Structure ----
-    kTag_FabricId                               = 1,    /**< [ uint ] Fabric ID. */
-    kTag_FabricKeys                             = 2,    /**< [ array ] List of FabricKey structures. */
+    kTag_FabricId                     = 1, /**< [ uint ] Fabric ID. */
+    kTag_FabricKeys                   = 2, /**< [ array ] List of FabricKey structures. */
 
     // ---- Context-specific Tags for FabricKey Structure ----
-    kTag_FabricKeyId                            = 1,    /**< [ uint ] Weave key ID for fabric key. */
-    kTag_EncryptionType                         = 2,    /**< [ uint ] Weave encryption type supported by the key. */
-    kTag_DataKey                                = 3,    /**< [ byte-string ] Data encryption key. */
-    kTag_IntegrityKey                           = 4,    /**< [ byte-string ] Data integrity key. */
-    kTag_KeyScope                               = 5,    /**< [ uint ] Enumerated value identifying the category of devices that can possess
-                                                                      the fabric key. */
-    kTag_RotationScheme                         = 6,    /**< [ uint ] Enumerated value identifying the rotation scheme for the key. */
-    kTag_RemainingLifeTime                      = 7,    /**< [ uint ] Remaining time (in seconds) until key expiration. Absent if lifetime
-                                                                      is indefinite or doesn't apply. */
-    kTag_RemainingReservedTime                  = 8     /**< [ uint ] Remaining time (in seconds) until key is eligible for use. Absent if
-                                                                      key can be used right away. */
+    kTag_FabricKeyId                  = 1, /**< [ uint ] Weave key ID for fabric key. */
+    kTag_EncryptionType               = 2, /**< [ uint ] Weave encryption type supported by the key. */
+    kTag_DataKey                      = 3, /**< [ byte-string ] Data encryption key. */
+    kTag_IntegrityKey                 = 4, /**< [ byte-string ] Data integrity key. */
+    kTag_KeyScope                     = 5, /**< [ uint ] Enumerated value identifying the category of devices that can possess
+                                                  the fabric key. */
+    kTag_RotationScheme               = 6, /**< [ uint ] Enumerated value identifying the rotation scheme for the key. */
+    kTag_RemainingLifeTime            = 7, /**< [ uint ] Remaining time (in seconds) until key expiration. Absent if lifetime
+                                                  is indefinite or doesn't apply. */
+    kTag_RemainingReservedTime        = 8  /**< [ uint ] Remaining time (in seconds) until key is eligible for use. Absent if key
+                                                  can be used right away. */
 };
-
+// clang-format on
 
 /**
  * Delegate class for implementing additional actions corresponding to Fabric Provisioning operations.
