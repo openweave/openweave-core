3675ef2ecef20cbde8a882a9ec3e81b89d4e97a5
Didi She <didis@google.com>
2021-01-13 19:19:34 -0800

    Add fileName and lineNumber in WeaveLog*
    Help debug and identify spammy log

--- src/lib/core/WeaveConfig.h
+++ src/lib/core/WeaveConfig.h
@@ -1883,6 +1883,17 @@
 
 
 /**
+ *  @def WEAVE_CONFIG_LOG_WITH_FILE_AND_LINE
+ *
+ *  @brief
+ *    If asserted (1), enable logging of file name and line number.
+ */
+#ifndef WEAVE_CONFIG_LOG_WITH_FILE_AND_LINE
+#define WEAVE_CONFIG_LOG_WITH_FILE_AND_LINE 0
+#endif // WEAVE_CONFIG_LOG_WITH_FILE_AND_LINE
+
+
+/**
  *  @def WEAVE_CONFIG_ENABLE_SERVICE_DIRECTORY
  *
  *  @brief
